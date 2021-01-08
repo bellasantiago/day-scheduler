412147dba003155eda79daaf9d171a53372aacfe
Isabella Santiago <isabellasantiago@Isabellas-MacBook-Pro.local>
2021-01-08 20:30:52 +1030

    Values are stored in the local storage and clear button working

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,41 @@
 // Sets the date on the scheduler as today's date
 $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
-$(".saveBtn").click(function(event) {
+//Fucntion to retrieve stored data from local server
+function getAll() {
+    $("#9 .toDo").val(localStorage.getItem("9"));
+    $("#10 .toDo").val(localStorage.getItem("10"));
+    $("#11 .toDo").val(localStorage.getItem("11"));
+    $("#12 .toDo").val(localStorage.getItem("12"));
+    $("#1 .toDo").val(localStorage.getItem("1"));
+    $("#2 .toDo").val(localStorage.getItem("2"));
+    $("#3 .toDo").val(localStorage.getItem("3"));
+    $("#4 .toDo").val(localStorage.getItem("4"));
+    $("#5 .toDo").val(localStorage.getItem("5"));
+};
+
+//Call retrieve the getAll function 
+getAll();
+
+
+//Function setting "clear all" button's action
+$(".clearBtn").click(function (event) {
     event.preventDefault()
-    
-    var task = $(".toDo");
-    var time = $(".time");
-    
-    localStorage.setItem(time, task);
-    
+
+    localStorage.clear();
+    getAll();
 });
+
+//Function setting "save" button's action
+$(".saveBtn").click(function (event) {
+    event.preventDefault()
+
+    //Set under the variable "Task" and "Time", the content of the text in the row as the value and the ID of the parent DIV as the Key
+    var task = $(this).siblings(".toDo").val();
+    var time = $(this).parent().attr("id");
+
+    //Store "Task" and "Time" in the local storage
+    localStorage.setItem(time, task);
+
+});
+
