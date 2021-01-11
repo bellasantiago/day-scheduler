8da8b32a74ae5cca2691bb869dfa4130efa02eed
Isabella Santiago <isabellasantiago@Isabellas-MacBook-Pro.local>
2021-01-11 18:21:22 +1030

    changed element ID

--- assets/script.js
+++ assets/script.js
@@ -47,14 +47,16 @@ $(".timeBlock").each(function () {
     //Reach the row's ID to retrieve row's relative time
     var storedTime = parseInt($(this).attr("id"));
 
+    console.log(storedTime , currentTime);
+
     //If the hour has already passed in comparison to current time, then addClass "PAST"
     if (storedTime < currentTime) {
         $(this).addClass("past");
         $(this).removeClass("present");
         $(this).removeClass("future");
     } else {
-        //If the hour has already passed in comparison to current time, then addClass "PRESENT"
         if (storedTime === currentTime) {
+            //If the hour has already passed in comparison to current time, then addClass "PRESENT"
             $(this).addClass("present");
             $(this).removeClass("past");
             $(this).removeClass("future");
